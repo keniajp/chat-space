deddc9ef3003580ab97f6cfe5cd97e36dbada233
keniajp <ilikeigo11@gmail.com>
2019-09-19 16:30:00 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.182.243.80*', user: 'ec2-user', roles: %w{app db web}
+server '18.182.243.80', user: 'ec2-user', roles: %w{app db web}
